b86708e7a971031b31547f6c36ec78b1f99a977a
mohammad <mohammadwesam13@gmail.com>
2026-03-30 21:11:50 +0300

    Initial commit

---
+++ src/main/java/com/example/nosql_database_management_system/NoSqlDatabaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.nosql_database_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlDatabaseManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoSqlDatabaseManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nosql_database_management_system/NoSqlDatabaseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nosql_database_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSqlDatabaseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
