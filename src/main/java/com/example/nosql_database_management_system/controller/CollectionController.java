ee6a4dc5d0b74f765e4aac0f5e440130fa0b227d
mohammad <mohammadwesam13@gmail.com>
2026-04-02 02:25:37 +0300

    refactor collection controller (add APIResponse)

--- src/main/java/com/example/nosql_database_management_system/controller/CollectionController.java
+++ src/main/java/com/example/nosql_database_management_system/controller/CollectionController.java
@@ -1,5 +1,6 @@
 package com.example.nosql_database_management_system.controller;
 
+import com.example.nosql_database_management_system.model.APIResponse;
 import com.example.nosql_database_management_system.model.CollectionSchema;
 import com.example.nosql_database_management_system.service.CollectionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +15,21 @@ public class CollectionController {
     private CollectionService service;
 
     @PostMapping("/createCollection/{DBName}/{CollectionName}")
-    public void createCol(
+    public APIResponse createCol(
             @PathVariable String DBName,
             @PathVariable String CollectionName,
             @RequestBody CollectionSchema schema
             ) throws IOException {
         service.createCol(DBName, CollectionName, schema);
+        return new APIResponse(200, "Collection created successfully");
     }
 
     @DeleteMapping("/deleteCollection/{DBName}/{CollectionName}")
-    public void deleteCol(
+    public APIResponse deleteCol(
             @PathVariable String DBName,
             @PathVariable String CollectionName
     ) {
         service.deleteCol(DBName, CollectionName);
+        return new APIResponse(200, "Collection deleted successfully");
     }
 }
