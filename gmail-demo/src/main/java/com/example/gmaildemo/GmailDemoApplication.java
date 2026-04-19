66887b4ddd61cb44fe678907366861c983a20790
mohammad <mohammadwesam13@gmail.com>
2026-04-19 23:23:15 +0300

    add gmail-demo skeleton

---
+++ gmail-demo/src/main/java/com/example/gmaildemo/GmailDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gmaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmailDemoApplication.class, args);
+    }
+
+}
